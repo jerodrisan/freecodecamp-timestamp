93cdccc10f0561b143639576f4e3460fc2926c33
jerodrisan <chukks@gmail.com>
2024-05-21 00:37:39 +0200

    first comit

--- index.js
+++ index.js
@@ -25,6 +25,68 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get("/api/", (req,res)=>{
+  //caso de enviar un valor de fecha vacio:
+  const date = new Date()
+  const formattedDate = date.toGMTString(date)
+  const timestamp = Date.parse(formattedDate)
+  res.json({unix: timestamp, utc:formattedDate})
+})
+
+
+
+app.get("/api/:date", function(req, res){
+  
+  let stringDate=req.params.date   
+
+  function usingDate (){
+ 
+      if (! stringDate.includes('-')){
+        //En caso de ser un timestamp
+        stringDate = Number (stringDate)     
+      }
+      const date = new Date (stringDate)
+      if (date=="Invalid Date"){
+        return res.json({error:"Invalid Date"})
+      }
+
+      const formattedDate = date.toGMTString(date)
+      const timestamp = Date.parse(formattedDate) //pasamos a timestamp
+     
+      res.json({unix: timestamp, utc: formattedDate}) 
+
+  }
+
+
+  function usingTimestamp(){
+
+      let timestamp = Date.parse(stringDate)
+
+      if ( isNaN(timestamp)){
+        console.log('es un timestamp y pasamos a numero ')
+        timestamp = Number(stringDate)     
+            
+      }
+      const date= new Date(timestamp)  
+
+      console.log(date)
+      if (date=="Invalid Date"){
+        return res.json({error:"Invalid Date"})
+      }
+      const formattedDate = date.toGMTString(date)
+      console.log(formattedDate)
+      res.json({unix: timestamp, utc: formattedDate})
+
+  }
+
+  usingDate()
+  //usingTimestamp()  
+});
+
+
+
+
+
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
